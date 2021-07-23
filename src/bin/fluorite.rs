87c00a36604286bfc50ea0d7a9ec48fd3cbdb111
Alyssa Riceman <alyssariceman@gmail.com>
2021-07-23 14:29:46 -0400

    allow multiline input

--- src/bin/fluorite.rs
+++ src/bin/fluorite.rs
@@ -1,7 +1,7 @@
 use argh::FromArgs;
 use fluorite::parse::parse_input;
 use fluorite::{format_string_with_results, format_string_with_rolls};
-use std::io::stdin;
+use std::io::{Read, stdin};
 
 /// Roll dice via string input.
 #[derive(FromArgs)]
@@ -20,23 +20,25 @@ fn main() {
         args.roll.join(" ")
     } else {
         let mut buffer = String::new();
-        match stdin().read_line(&mut buffer) {
+        match stdin().read_to_string(&mut buffer) {
             Ok(_) => (),
             Err(_) => println!("Failed to read from stdin."),
         };
         buffer
     };
 
-    match parse_input(&input) {
-        Ok(results) => {
-            if args.verbose {
-                println!("Original roll text: {}", format_string_with_rolls(&results.processed_string, results.original_roll_texts));
-                println!("With results: {}", format_string_with_results(&results.processed_string, results.rolls));
-                println!("Total: {}", results.value);
-            } else {
-                println!("{}", results.value);
+    for line in input.split('\n').filter(|line| line != &"") {
+        match parse_input(&line) {
+            Ok(results) => {
+                if args.verbose {
+                    println!("Original roll text: {}", format_string_with_rolls(&results.processed_string, results.original_roll_texts));
+                    println!("With results: {}", format_string_with_results(&results.processed_string, results.rolls));
+                    println!("Total: {}", results.value);
+                } else {
+                    println!("{}", results.value);
+                }
             }
+            Err(e) => panic!("{}", e),
         }
-        Err(e) => println!("{}", e),
     }
 }
